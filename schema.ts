31b229463794bd6524d7cd1e68f8208fc7820d72
Mike Bendorf <mikebendorf11@gmail.com>
2022-08-24 11:51:02 -0700

    crud for customer, order, product
    Schema for mutations and a method to output SDL
    /graphql graphqlHTTP handle
    	rootValue has queries and mutations
    	an CreateOrder returns the Customer and Products props

--- schema.ts
+++ schema.ts
@@ -2,5 +2,8 @@ const { buildSchema } = require('graphql');
 const path = require('path')
 const fs = require('fs')
 const schemaAsString = fs.readFileSync('./schema.graphql', 'utf8')
+ 
 
+//  printSchema(schema) method should reverse to SDL
 module.exports.build = buildSchema(schemaAsString)
+ 
--- server.ts
+++ server.ts
@@ -4,26 +4,73 @@ const schema = require('./schema')
 const cors = require('cors');
 const mongoDB = require('./data/db')
 const sample = require('./data/sample')
-const Resolver = require('./data/resolvers')
+const {ObjectId }  = require('mongoDB')
 const app = express()
 
 mongoDB.connect().then(async db=>{
   let customers = db.collection('customer')
-  let products = db.collection('product')
+  let products = db.collection('product') 
+  let orders = db.collection('orders')
+  //load sample collections
   if(!await customers.count()) {
-    await customers.insertMany(sample.Customer)
+    await customers.insertMany(sample.Customer) 
   }
   if(!await products.count()){
     await products.insertMany(sample.Product)
   }
   app.use(cors())
-  //console.log(await customers.find({}).toArray())
-  app.use('/graphql', graphqlHTTP({ 
+  app.use('/graphql', graphqlHTTP({ //transforms schema SDL and defines crud methods
       schema: schema.build,
       graphiql:true,
       rootValue: {
         customers: ()=>  customers.find({}).toArray(),
-        products: ()=>  products.find({}).toArray()
+        products: ()=>  products.find({}).toArray(),
+        //async needed for detailed mutation
+        createOrder: async ({productId, customerId, price}) => {
+          let cid = ObjectId(customerId) //type needed for aggr
+          let pids1 = productId.map(pid=>ObjectId(pid))
+          //input is only matching the id
+          let orderRequest = await orders.insertOne({
+            price, 
+            customerId: cid, 
+            productId: pids1
+          })
+          //then lookback doesn't need 3xfind() (faster)
+          let agg = await orders.aggregate([
+            {
+              $match: {
+                _id: orderRequest.insertedId
+              }
+            },{
+              $lookup: {
+                from: 'customer',
+                localField: 'customerId',
+                foreignField: '_id',
+                as: 'customer'
+              }
+            }, 
+            {
+              $lookup: {
+                from: 'product',
+                localField: 'productId',
+                foreignField: '_id',
+                as: 'products'
+              }
+            },
+            {
+              $project: { //grab only marked with 1
+                _id:0, products:1, customer:1
+              }
+            }
+          ])
+          let result = await agg.toArray()
+          //console.log('hi there')
+          return { //output need to match order type
+            _id: orderRequest.insertedId,
+            customer: result[0].customer[0],
+            products: result[0].products
+          }
+        }
       }
   }))
   app.listen(4000, () => {
